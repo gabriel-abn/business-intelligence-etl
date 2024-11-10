1517a29e0708bbbecedf106a38cf13f9c943ca63
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-11-09 22:30:40 -0300

    fix: updated OLAP schema file for correct table creation order

--- database/olap/1_olap.sql
+++ database/olap/1_olap.sql
@@ -1,53 +1,5 @@
 CREATE SCHEMA olap;
 
-CREATE TABLE olap.fact_automation_billing (
-  bill_id INTEGER PRIMARY KEY,
-  auto_door_id INTEGER REFERENCES dim_auto_door(id),
-  sprinkler_id INTEGER REFERENCES dim_sprinkler(id),
-  light_id INTEGER REFERENCES dim_light(id),
-  value FLOAT,
-  consumption FLOAT,
-  measurement_unit VARCHAR(50),
-  due_date TIMESTAMP,
-  paid BOOLEAN,
-  issued_at TIMESTAMP,
-  paid_at TIMESTAMP,
-  time_id INTEGER REFERENCES dim_time(id)
-);
-
--- Criação da tabela de dimensões: Portas Automáticas
-CREATE TABLE olap.dim_auto_door (
-  id INTEGER PRIMARY KEY,
-  service_id INTEGER NOT NULL,
-  model VARCHAR(255),
-  avg_energy_consumption FLOAT,
-  local_id INTEGER REFERENCES dim_local(id),
-  status VARCHAR(255),
-  created_at TIMESTAMP
-);
-
--- Criação da tabela de dimensões: Irrigadores
-CREATE TABLE olap.dim_sprinkler (
-  id INTEGER PRIMARY KEY,
-  irrigation_point_id INTEGER NOT NULL,
-  sprinklers_count INTEGER,
-  avg_water_consumption FLOAT,
-  local_id INTEGER REFERENCES dim_local(id),
-  status VARCHAR(255),
-  created_at TIMESTAMP
-);
-
--- Criação da tabela de dimensões: Luzes Automáticas
-CREATE TABLE olap.dim_light (
-  id INTEGER PRIMARY KEY,
-  service_id INTEGER NOT NULL,
-  model VARCHAR(255),
-  avg_energy_consumption FLOAT,
-  local_id INTEGER REFERENCES dim_local(id),
-  status VARCHAR(255),
-  created_at TIMESTAMP
-);
-
 -- Criação da tabela de dimensões: Locais
 CREATE TABLE olap.dim_local (
   id INTEGER PRIMARY KEY,
@@ -65,4 +17,62 @@ CREATE TABLE olap.dim_time (
   week INTEGER,
   day_of_week VARCHAR(15),
   is_weekend BOOLEAN
+);
+
+-- Criação da tabela de dimensões: Irrigadores
+CREATE TABLE olap.dim_sprinkler (
+  id INTEGER PRIMARY KEY,
+  irrigation_point_id INTEGER NOT NULL,
+  sprinklers_count INTEGER,
+  avg_water_consumption FLOAT,
+  local_id INTEGER,
+  status VARCHAR(255),
+  created_at TIMESTAMP,
+  --
+  FOREIGN KEY (local_id) REFERENCES dim_local(id)
+);
+
+-- Criação da tabela de dimensões: Luzes Automáticas
+CREATE TABLE olap.dim_light (
+  id INTEGER PRIMARY KEY,
+  service_id INTEGER NOT NULL,
+  model VARCHAR(255),
+  avg_energy_consumption FLOAT,
+  local_id INTEGER,
+  status VARCHAR(255),
+  created_at TIMESTAMP,
+  --
+  FOREIGN KEY (local_id) REFERENCES dim_local(id)
+);
+
+CREATE TABLE olap.dim_auto_door (
+  id INTEGER PRIMARY KEY,
+  service_id INTEGER NOT NULL,
+  model VARCHAR(255),
+  avg_energy_consumption FLOAT,
+  local_id INTEGER,
+  status VARCHAR(255),
+  created_at TIMESTAMP,
+  --
+  FOREIGN KEY (local_id) REFERENCES dim_local(id)
+);
+
+CREATE TABLE olap.fact_automation_billing (
+  bill_id INTEGER PRIMARY KEY,
+  auto_door_id INTEGER,
+  sprinkler_id INTEGER,
+  light_id INTEGER,
+  value FLOAT,
+  consumption FLOAT,
+  measurement_unit VARCHAR(50),
+  due_date TIMESTAMP,
+  paid BOOLEAN,
+  issued_at TIMESTAMP,
+  paid_at TIMESTAMP,
+  time_id INTEGER,
+  --
+  FOREIGN KEY (auto_door_id) REFERENCES dim_auto_door(id),
+  FOREIGN KEY (sprinkler_id) REFERENCES dim_sprinkler(id),
+  FOREIGN KEY (light_id) REFERENCES dim_light(id),
+  FOREIGN KEY (time_id) REFERENCES dim_time(id)
 );
